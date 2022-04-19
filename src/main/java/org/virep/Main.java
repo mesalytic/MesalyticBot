9a258992cb5a92263c7611391255363680cad5f2
MesaVirep <atamatori56@gmail.com>
2022-04-19 06:11:29 +0200

    added config files + main client class

--- src/main/java/org/virep/Main.java
+++ src/main/java/org/virep/Main.java
@@ -1,7 +1,12 @@
 package org.virep;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws Exception {
+        JDA api = JDABuilder
+                .createDefault(Config.get("TOKEN"))
+                .build();
     }
 }
