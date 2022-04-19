d4d703aac924bf5c1b874180a5a3c0241211eb14
MesaVirep <atamatori56@gmail.com>
2022-04-19 05:34:51 +0200

    added java-dotenv

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     implementation("net.dv8tion:JDA:5.0.0-alpha.10")
+    implementation("io.github.cdimascio:java-dotenv:5.2.2")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
