8ddf876d0b0495cf5eb07023c6b04be0263a87fb
MesaVirep <atamatori56@gmail.com>
2022-04-20 23:27:03 +0200

    feat(TicTacToe): tictactoe now checks if user is mentionned
    it also :
    - checks if the mentionned user is a bot
    - checks if a game is currently running in the channel
    - ask the mentionned user if he wants to play
    - checks if the user that pressed a button is a player
    - alternates current player check depending on the turn

--- src/main/java/org/virep/jdabot/commands/TTTCommand.java
+++ src/main/java/org/virep/jdabot/commands/TTTCommand.java
@@ -1,13 +1,24 @@
 package org.virep.jdabot.commands;
 
 import net.dv8tion.jda.api.entities.Emoji;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import org.virep.jdabot.slashcommandhandler.SlashCommand;
 
-import java.util.Arrays;
+import java.util.*;
+
+/*
+TODO: Switch from X to O depending on the player.
+
+TODO: Check if its the player's turn when a button is pressed.
+
+TODO: Check if someone wins each round.
+
+*/
 
 public class TTTCommand extends SlashCommand {
 
@@ -17,26 +28,44 @@ public class TTTCommand extends SlashCommand {
                 new OptionData(OptionType.USER, "opponent", "Ping the opponent"));
     }
 
+    private static final int[][] board = new int[3][3];
+    public static Map<Long, int[][]> boards = new HashMap<>();
+    public static Map<Long, long[]> players = new HashMap<>();
+    public static Map<Long, Long> playersTurn = new HashMap<>();
+    public static long playerID;
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-        int[][] board = new int[3][3];
-
-        System.out.println(Arrays.deepToString(board));
-
-        StringBuilder replyContent = new StringBuilder();
-        for (int i = 0; i < 3; i++) {
-            if (i > 0) replyContent.append("\n");
-            for (int j = 0; j < 3; j++) {
-                System.out.println(i + " " + j);
-                switch (board[i][j]) {
-                    case 0 -> replyContent.append("\u2B1B");
-                    case 1 -> replyContent.append("\u274C");
-                    case 2 -> replyContent.append("\u2B55");
-                }
-            }
+        if (event.getOption("opponent") == null) {
+            event.reply("Vous devez mentionner quelqu'un! `/tictactoe opponent`").setEphemeral(true).queue();
+            return;
         }
 
-        event.reply(replyContent.toString())
+        /*if (event.getOption("opponent").getAsUser().isBot()) {
+            event.reply("Vous ne pouvez pas jouer contre un bot.").setEphemeral(true).queue();
+            return;
+        }*/
+
+        if (boards.containsKey(event.getChannel().getIdLong())) event.reply("Une partie est deja en cours!").setEphemeral(true).queue();
+        else {
+            players.put(event.getChannel().getIdLong(), new long[]{Objects.requireNonNull(event.getMember()).getIdLong(), Objects.requireNonNull(event.getOption("opponent")).getAsUser().getIdLong() });
+
+            User opponent = Objects.requireNonNull(event.getOption("opponent")).getAsUser();
+
+            event.reply(opponent.getAsMention() + ", souhaitez vous jouer au morpion contre " + Objects.requireNonNull(event.getMember()).getAsMention() + " ?")
+                    .addActionRow(
+                            Button.primary("tictactoeAccept", "Accepter"),
+                            Button.danger("tictactoeRefuse", "Refuser")
+                    )
+                    .queue();
+        }
+    }
+
+    public static void play(ButtonInteractionEvent event, long playerID) {
+        boards.put(event.getChannel().getIdLong(), board);
+
+        long[] playersArray = players.get(event.getChannel().getIdLong());
+
+        event.reply(replyBoard(board, playerID))
                 .addActionRow(
                         Button.secondary("tictactoeButton1", Emoji.fromUnicode("\u0031\u20E3")),
                         Button.secondary("tictactoeButton2", Emoji.fromUnicode("\u0032\u20E3")),
@@ -53,5 +82,26 @@ public class TTTCommand extends SlashCommand {
                         Button.secondary("tictactoeButton9", Emoji.fromUnicode("\u0039\u20E3") )
                 )
                 .queue();
+    }
+
+    public static String replyBoard(int[][] boardArray, long playerID) {
+        System.out.println(Arrays.deepToString(boardArray));
+
+        StringBuilder replyContent = new StringBuilder();
+
+        replyContent.append("<@").append(playerID).append(">, a toi de jouer!");
+
+        for (int i = 0; i < 3; i++) {
+            if (i > 0) replyContent.append("\n");
+            for (int j = 0; j < 3; j++) {
+                System.out.println(i + " " + j);
+                switch (boardArray[i][j]) {
+                    case 0 -> replyContent.append("\u2B1B");
+                    case 1 -> replyContent.append("\u274C");
+                    case 2 -> replyContent.append("\u2B55");
+                }
+            }
+        }
+        return replyContent.toString();
     }
 }
--- src/main/java/org/virep/jdabot/listeners/SlashListener.java
+++ src/main/java/org/virep/jdabot/listeners/SlashListener.java
@@ -1,16 +1,19 @@
 package org.virep.jdabot.listeners;
 
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
+import org.virep.jdabot.commands.TTTCommand;
 import org.virep.jdabot.slashcommandhandler.SlashCommand;
 import org.virep.jdabot.slashcommandhandler.SlashHandler;
 
 import javax.annotation.Nonnull;
 import java.util.Map;
+import java.util.Objects;
 
 public class SlashListener extends ListenerAdapter {
     private final SlashHandler slashHandler;
-
     public SlashListener(SlashHandler slashHandler) {
         this.slashHandler = slashHandler;
     }
@@ -22,6 +25,61 @@ public class SlashListener extends ListenerAdapter {
 
         if (commandMap.containsKey(commandName)) {
             commandMap.get(commandName).execute(event);
+        }
+    }
+
+    @Override
+    public void onButtonInteraction(@Nonnull ButtonInteractionEvent event) {
+        Button button = event.getButton();
+        System.out.println(event.getInteraction());
+
+        if (Objects.equals(button.getId(), "tictactoeAccept")) {
+            TTTCommand.playersTurn.put(event.getChannel().getIdLong(), TTTCommand.players.get(event.getChannel().getIdLong())[0]);
+            TTTCommand.play(event, TTTCommand.playersTurn.get(event.getChannel().getIdLong()));
+        }
+
+
+
+
+        if (Objects.requireNonNull(button.getId()).startsWith("tictactoeButton")) {
+
+            long playerOneID = TTTCommand.players.get(event.getChannel().getIdLong())[0];
+            long playerTwoID = TTTCommand.players.get(event.getChannel().getIdLong())[1];
+
+            if (Objects.requireNonNull(event.getMember()).getIdLong() != playerOneID || event.getMember().getIdLong() != playerOneID) {
+                event.reply("Tu n'es pas de cette partie!").setEphemeral(true).queue();
+                return;
+            }
+
+            int[][] board = TTTCommand.boards.get(event.getChannel().getIdLong());
+
+            System.out.println("PlayerOneID " + playerOneID);
+            System.out.println("PlayerTwoID " + playerTwoID);
+
+            switch (button.getId()) {
+                case "tictactoeButton1" -> board[0][0] = 1;
+                case "tictactoeButton2" -> board[0][1] = 1;
+                case "tictactoeButton3" -> board[0][2] = 1;
+                case "tictactoeButton4" -> board[1][0] = 1;
+                case "tictactoeButton5" -> board[1][1] = 1;
+                case "tictactoeButton6" -> board[1][2] = 1;
+                case "tictactoeButton7" -> board[2][0] = 1;
+                case "tictactoeButton8" -> board[2][1] = 1;
+                case "tictactoeButton9" -> board[2][2] = 1;
+            }
+
+            long[] playersArray = TTTCommand.players.get(event.getChannel().getIdLong());
+
+            if (TTTCommand.playersTurn.get(event.getChannel().getIdLong()) == playersArray[0]) {
+                TTTCommand.playersTurn.remove(event.getChannel().getIdLong());
+                TTTCommand.playersTurn.put(event.getChannel().getIdLong(), playersArray[1]);
+            } else {
+                TTTCommand.playersTurn.remove(event.getChannel().getIdLong());
+                TTTCommand.playersTurn.put(event.getChannel().getIdLong(), playersArray[0]);
+            }
+
+            String replyBoard = TTTCommand.replyBoard(board, TTTCommand.playersTurn.get(event.getChannel().getIdLong()));
+            event.editMessage(replyBoard).queue();
         }
     }
 }
