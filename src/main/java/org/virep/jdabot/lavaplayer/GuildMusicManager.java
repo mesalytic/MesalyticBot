5474502b4b8a06801862a753e34903a2f34cafbc
MesaVirep <atamatori56@gmail.com>
2022-04-22 06:47:56 +0200

    feat(lavaplayer): added player managers

---
+++ src/main/java/org/virep/jdabot/lavaplayer/GuildMusicManager.java
@@ -0,0 +1,22 @@
+package org.virep.jdabot.lavaplayer;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+
+public class GuildMusicManager {
+    public final AudioPlayer player;
+    public final TrackScheduler scheduler;
+    private final AudioPlayerSendHandler sendHandler;
+
+    public GuildMusicManager(AudioPlayerManager manager) {
+        this.player = manager.createPlayer();
+        this.scheduler = new TrackScheduler(this.player);
+        this.player.addListener(this.scheduler);
+        this.sendHandler = new AudioPlayerSendHandler(this.player);
+    }
+
+
+    public AudioPlayerSendHandler getSendHandler() {
+        return sendHandler;
+    }
+}
---
+++ src/main/java/org/virep/jdabot/lavaplayer/PlayerManager.java
@@ -0,0 +1,77 @@
+package org.virep.jdabot.lavaplayer;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class PlayerManager {
+    private static PlayerManager INSTANCE;
+
+    private final Map<Long, GuildMusicManager> musicManagers;
+    private final AudioPlayerManager audioPlayerManager;
+
+    public PlayerManager() {
+        this.musicManagers = new HashMap<>();
+        this.audioPlayerManager = new DefaultAudioPlayerManager();
+    }
+
+    public GuildMusicManager getMusicManager(Guild guild) {
+        return this.musicManagers.computeIfAbsent(guild.getIdLong(), (guildID) -> {
+            final GuildMusicManager guildMusicManager = new GuildMusicManager(this.audioPlayerManager);
+
+            guild.getAudioManager().setSendingHandler(guildMusicManager.getSendHandler());
+
+            return guildMusicManager;
+        });
+    }
+
+    public void loadAndPlay(SlashCommandInteractionEvent event, String trackURL) {
+        final GuildMusicManager guildMusicManager = this.getMusicManager(Objects.requireNonNull(event.getGuild()));
+
+        this.audioPlayerManager.loadItemOrdered(guildMusicManager, trackURL, new AudioLoadResultHandler() {
+            @Override
+            public void trackLoaded(AudioTrack track) {
+                guildMusicManager.scheduler.queue(track);
+
+                String content = "Adding to Queue:" +
+                        track.getInfo().title +
+                        " by " +
+                        track.getInfo().author;
+
+                event.reply(content).queue();
+            }
+
+            @Override
+            public void playlistLoaded(AudioPlaylist playlist) {
+
+            }
+
+            @Override
+            public void noMatches() {
+
+            }
+
+            @Override
+            public void loadFailed(FriendlyException exception) {
+
+            }
+        });
+    }
+
+    public static PlayerManager getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new PlayerManager();
+        }
+
+        return INSTANCE;
+    }
+}
