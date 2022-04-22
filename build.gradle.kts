cc1ee31a31ba0f42638ebe53c80c0d7fb7859b00
MesaVirep <atamatori56@gmail.com>
2022-04-22 05:36:11 +0200

    chore(impl): added lavaplayer

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,11 +7,13 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://m2.dv8tion.net/releases")
 }
 
 dependencies {
     implementation("net.dv8tion:JDA:5.0.0-alpha.10")
     implementation("io.github.cdimascio:java-dotenv:5.2.2")
+    implementation ("com.sedmelluq:lavaplayer:1.3.77")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
