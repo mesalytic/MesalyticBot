8fd1b68922e65276c9c1cc2247e25dea54889cd3
Alice <atamatori56@gmail.com>
2022-04-26 13:43:34 +0200

    ref: renamed MessageListener in EventListener

--- src/main/java/org/virep/jdabot/listeners/EventListener.java
+++ src/main/java/org/virep/jdabot/listeners/EventListener.java
@@ -1,11 +1,17 @@
 package org.virep.jdabot.listeners;
 
+import lavalink.client.io.jda.JdaLavalink;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.virep.jdabot.Config;
 
-public class MessageListener extends ListenerAdapter {
+import java.net.URI;
+
+public class EventListener extends ListenerAdapter {
+
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
         if (event.getAuthor().isBot()) return;
