b8210d0f9068fa117e77cc3babe40f590e294d79
Alice <atamatori56@gmail.com>
2022-04-27 06:26:18 +0200

    chore: updated JDA 5 to alpha 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,11 @@ repositories {
 }
 
 dependencies {
-    implementation("net.dv8tion:JDA:5.0.0-alpha.10")
+    implementation("net.dv8tion:JDA:5.0.0-alpha.11")
     implementation("io.github.cdimascio:java-dotenv:5.2.2")
     implementation ("com.sedmelluq:lavaplayer:1.3.77")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     implementation("com.github.Xirado:Lavalink-Client:master-SNAPSHOT")
 }
 
