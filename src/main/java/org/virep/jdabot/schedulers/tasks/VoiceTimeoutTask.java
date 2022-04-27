6df8bc9a22c98944204ad9849fdd332a50ab60bb
Alice <atamatori56@gmail.com>
2022-04-27 06:04:25 +0200

    feat: added voice disconnection timeout
    when there's no tracks available in the queue, the bot will stay in VC for 5 mins and then disconnect, unless a new track is added in between

---
+++ src/main/java/org/virep/jdabot/schedulers/tasks/VoiceTimeoutTask.java
@@ -0,0 +1,22 @@
+package org.virep.jdabot.schedulers.tasks;
+
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import org.virep.jdabot.commands.StopCommand;
+import org.virep.jdabot.schedulers.Task;
+
+public class VoiceTimeoutTask implements Task {
+    private final Guild guild;
+    private final GuildVoiceState voiceState;
+
+    public VoiceTimeoutTask(Guild guild) {
+        this.guild = guild;
+        this.voiceState = guild.getSelfMember().getVoiceState();
+    }
+    @Override
+    public void run() {
+        if (voiceState != null && voiceState.inAudioChannel()) {
+            new StopCommand().disconnect(guild);
+        }
+    }
+}
