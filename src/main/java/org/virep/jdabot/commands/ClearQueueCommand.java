1adbc7be56b0c442dcb6bd96b52a348520c6222b
Alice <atamatori56@gmail.com>
2022-05-10 08:55:26 +0200

    feat: add clear queue command

---
+++ src/main/java/org/virep/jdabot/commands/ClearQueueCommand.java
@@ -0,0 +1,56 @@
+package org.virep.jdabot.commands;
+
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.virep.jdabot.lavaplayer.AudioManagerController;
+import org.virep.jdabot.lavaplayer.GuildAudioManager;
+import org.virep.jdabot.lavaplayer.TrackScheduler;
+import org.virep.jdabot.slashcommandhandler.SlashCommand;
+
+import java.util.Objects;
+
+public class ClearQueueCommand extends SlashCommand {
+    public ClearQueueCommand() {
+        super("clearqueue", "Clears the current queue.");
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        Guild guild = event.getGuild();
+
+        GuildAudioManager manager = AudioManagerController.getGuildAudioManager(guild);
+        TrackScheduler trackScheduler = manager.getScheduler();
+
+        assert guild != null;
+
+        final GuildVoiceState selfVoiceState = guild.getSelfMember().getVoiceState();
+        final GuildVoiceState memberVoiceState = Objects.requireNonNull(event.getMember()).getVoiceState();
+
+        assert memberVoiceState != null;
+        assert selfVoiceState != null;
+
+        if (memberVoiceState.getChannel() == null) {
+            event.reply("You are not in a voice channel!").setEphemeral(true).queue();
+            return;
+        }
+
+        if (!selfVoiceState.inAudioChannel()) {
+            event.reply("I'm currently not playing any music!").setEphemeral(true).queue();
+            return;
+        }
+
+        if (Objects.requireNonNull(selfVoiceState.getChannel()).getIdLong() != memberVoiceState.getChannel().getIdLong()) {
+            event.reply("You are not in the same channel as me!").setEphemeral(true).queue();
+            return;
+        }
+
+        if (trackScheduler.queue.isEmpty()) {
+            event.reply("Nothing is in the queue right now.").setEphemeral(true).queue();
+            return;
+        }
+
+        trackScheduler.queue.clear();
+        event.reply("The queue has been cleared.").queue();
+    }
+}
