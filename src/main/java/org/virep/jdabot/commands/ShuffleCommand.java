5d6263c0729c57d03b97c2139211d850e0922bf3
Alice <atamatori56@gmail.com>
2022-05-23 04:05:50 +0200

    feat: add shuffle command

---
+++ src/main/java/org/virep/jdabot/commands/ShuffleCommand.java
@@ -0,0 +1,53 @@
+package org.virep.jdabot.commands;
+
+import lavalink.client.player.LavalinkPlayer;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import org.virep.jdabot.lavaplayer.AudioManagerController;
+import org.virep.jdabot.lavaplayer.GuildAudioManager;
+import org.virep.jdabot.slashcommandhandler.SlashCommand;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class ShuffleCommand extends SlashCommand {
+    public ShuffleCommand() {
+        super("shuffle", "Shuffles the queue");
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        Guild guild = event.getGuild();
+
+        GuildAudioManager manager = AudioManagerController.getGuildAudioManager(guild);
+
+        assert guild != null;
+
+        final GuildVoiceState selfVoiceState = guild.getSelfMember().getVoiceState();
+        final GuildVoiceState memberVoiceState = Objects.requireNonNull(event.getMember()).getVoiceState();
+
+        assert memberVoiceState != null;
+        assert selfVoiceState != null;
+
+        if (memberVoiceState.getChannel() == null) {
+            event.reply("You are not in a voice channel!").setEphemeral(true).queue();
+            return;
+        }
+
+        if (!selfVoiceState.inAudioChannel()) {
+            event.reply("I'm currently not playing any music!").setEphemeral(true).queue();
+            return;
+        }
+
+        if (Objects.requireNonNull(selfVoiceState.getChannel()).getIdLong() != memberVoiceState.getChannel().getIdLong()) {
+            event.reply("You are not in the same channel as me!").setEphemeral(true).queue();
+            return;
+        }
+
+        Collections.shuffle((List<?>) manager.getScheduler().queue);
+
+        event.reply("The queue has successfully been shuffled!").queue();
+    }
+}
