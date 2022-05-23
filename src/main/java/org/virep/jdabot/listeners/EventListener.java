5ed2aa4082fa9b0dd3646f8fadcce46ce2039549
Alice <atamatori56@gmail.com>
2022-05-23 03:05:28 +0200

    test: on shutdowns bot should leave vcs

--- src/main/java/org/virep/jdabot/listeners/EventListener.java
+++ src/main/java/org/virep/jdabot/listeners/EventListener.java
@@ -1,11 +1,40 @@
 package org.virep.jdabot.listeners;
 
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.audio.hooks.ConnectionStatus;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.ShutdownEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.managers.AudioManager;
+import org.virep.jdabot.lavaplayer.AudioLoadHandler;
+import org.virep.jdabot.lavaplayer.GuildAudioManager;
 
 public class EventListener extends ListenerAdapter {
+
+    @Override
+    public void onShutdown(ShutdownEvent event) {
+        JDA jda = event.getJDA();
+
+        if (jda.getStatus() != JDA.Status.SHUTTING_DOWN) {
+            jda.getGuilds().forEach(g -> {
+                AudioManager manager = g.getAudioManager();
+                manager.getConnectionStatus();
+                if (manager.getConnectionStatus() == ConnectionStatus.CONNECTED) {
+                    g.getAudioManager().closeAudioConnection();
+
+                    GuildAudioManager gam = new GuildAudioManager(g);
+                    gam.getPlayer().getLink().destroy();
+
+                }
+            });
+            jda.shutdown();
+        }
+
+        System.out.println("Shutdown !");
+        System.exit(0);
+    }
 
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
@@ -14,9 +43,21 @@ public class EventListener extends ListenerAdapter {
         Message message = event.getMessage();
         String content = message.getContentRaw();
 
-        if (content.equals("jda!ping")) {
-            MessageChannel channel = event.getChannel();
-            channel.sendMessage("Pong!").queue();
+        if (content.equals("jda!shutdown")) {
+            JDA jda = event.getJDA();
+
+            if (jda.getStatus() != JDA.Status.SHUTTING_DOWN) {
+                jda.getGuilds().forEach(g -> {
+                    g.getAudioManager().closeAudioConnection();
+
+                    GuildAudioManager gam = new GuildAudioManager(g);
+                    gam.getPlayer().getLink().destroy();
+                });
+
+                event.getMessage().reply("Shutdown en cours").queue();
+
+                jda.shutdown();
+            }
         }
     }
 }
