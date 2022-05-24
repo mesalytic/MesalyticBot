4eaf1863d57bce97d62af057364501be2f4fb8b4
Alice <atamatori56@gmail.com>
2022-05-24 22:14:38 +0200

    test(deps): forked kittybot-org ll client to support jda5

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,7 @@ repositories {
 dependencies {
     implementation("net.dv8tion:JDA:5.0.0-alpha.12")
     implementation("io.github.cdimascio:java-dotenv:5.2.2")
-    implementation("com.github.KittyBot-Org:Lavalink-Client:-SNAPSHOT")
-    // implementation("com.github.walkyst:lavaplayer-fork:1.3.97")
+    implementation("com.github.chocololat:Lavalink-Client:44164aae3f")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 
