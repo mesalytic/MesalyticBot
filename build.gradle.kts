dba515f2b985a55cb2649608c4a7c4f79fb6adc0
Alice <atamatori56@gmail.com>
2022-05-24 21:22:34 +0200

    test(deps): replaced xirado with kittybot-org LL client
    the fork is LP free, would need a lot of refactor but it would be easier in the long run

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,10 +14,8 @@ repositories {
 dependencies {
     implementation("net.dv8tion:JDA:5.0.0-alpha.12")
     implementation("io.github.cdimascio:java-dotenv:5.2.2")
-    implementation("com.github.Xirado:Lavalink-Client:master-SNAPSHOT") {
-        exclude (group = "com.sedmelluq", module = "lavaplayer")
-    }
-    implementation("com.github.walkyst:lavaplayer-fork:1.3.97")
+    implementation("com.github.KittyBot-Org:Lavalink-Client:-SNAPSHOT")
+    // implementation("com.github.walkyst:lavaplayer-fork:1.3.97")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 
