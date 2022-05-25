26607ab786fd9f45353e913a55f7e6c8bbb8770f
Alice <atamatori56@gmail.com>
2022-05-25 05:11:22 +0200

    feat: added remote and attachment audio files support

--- src/main/java/org/virep/jdabot/commands/PlayCommand.java
+++ src/main/java/org/virep/jdabot/commands/PlayCommand.java
@@ -2,6 +2,7 @@ package org.virep.jdabot.commands;
 
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.entities.Message.Attachment;
 import net.dv8tion.jda.api.entities.VoiceChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
@@ -25,7 +26,9 @@ public class PlayCommand extends SlashCommand {
                 new SubcommandData[] {
                         new SubcommandData("youtube", "Play YouTube urls!").addOption(OptionType.STRING, "url", "YouTube Video or Playlist URL").addOption(OptionType.STRING, "search", "Search string"),
                         new SubcommandData("soundcloud", "Play SoundCloud urls!").addOption(OptionType.STRING, "url", "SoundCloud URL").addOption(OptionType.STRING, "search", "Search string"),
-                        new SubcommandData("spotify", "Play Spotify URLs").addOption(OptionType.STRING, "url", "url").addOption(OptionType.STRING, "search", "search")
+                        new SubcommandData("spotify", "Play Spotify URLs").addOption(OptionType.STRING, "url", "url").addOption(OptionType.STRING, "search", "search"),
+                        new SubcommandData("other", "URLs that are not from Spotify/YouTube/SoundCloud").addOption(OptionType.STRING, "url", "URL string", true),
+                        new SubcommandData("file", "MP3 file").addOption(OptionType.ATTACHMENT, "file", "file", true)
                 }
         );
     }
@@ -45,29 +48,43 @@ public class PlayCommand extends SlashCommand {
 
         OptionMapping urlOption = event.getOption("url");
         OptionMapping searchOption = event.getOption("search");
+        OptionMapping fileOption = event.getOption("file");
 
         String result = "";
 
-        if (urlOption == null && searchOption == null) {
-            event.reply("You must specify a URL or a search query.").setEphemeral(true).queue();
+        if (urlOption == null && searchOption == null && fileOption == null) {
+            event.reply("You must select at least an option.").setEphemeral(true).queue();
             return;
-        } else {
-            if (urlOption != null) {
-                try {
-                    new URL(urlOption.getAsString());
-                    result = urlOption.getAsString();
-                } catch (MalformedURLException e) {
-                    event.reply("You must specify a valid URL.").setEphemeral(true).queue();
-                    return;
-                }
-            } else {
-                assert event.getSubcommandName() != null;
-                switch (event.getSubcommandName()) {
-                    case "youtube" -> result = "ytsearch:" + searchOption.getAsString();
-                    case "soundcloud" -> result = "scsearch:" + searchOption.getAsString();
-                    case "spotify" -> result = "spsearch:" + searchOption.getAsString();
-                }
+        }
+
+        if (urlOption != null && searchOption != null) {
+            event.reply("You must select only one option. (either `search` or `url`).").setEphemeral(true).queue();
+            return;
+        }
+
+        if (urlOption != null) {
+            try {
+                new URL(urlOption.getAsString());
+                result = urlOption.getAsString();
+            } catch (MalformedURLException e) {
+                event.reply("You must specify a valid URL.").setEphemeral(true).queue();
+                return;
             }
+        }
+
+        if (searchOption != null) {
+            assert event.getSubcommandName() != null;
+            switch (event.getSubcommandName()) {
+                case "youtube" -> result = "ytsearch:" + searchOption.getAsString();
+                case "soundcloud" -> result = "scsearch:" + searchOption.getAsString();
+                case "spotify" -> result = "spsearch:" + searchOption.getAsString();
+            }
+        }
+
+        if (fileOption != null) {
+            Attachment attachment = fileOption.getAsAttachment();
+
+            result = attachment.getProxyUrl();
         }
 
         if (memberVoiceState.getChannel() == null) {
