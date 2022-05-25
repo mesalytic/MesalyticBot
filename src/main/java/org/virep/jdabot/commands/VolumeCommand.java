1da92a7d35768dfb70626cf9f8a597a494b96f7a
Alice <atamatori56@gmail.com>
2022-05-25 19:17:58 +0200

    feat: add volume command

---
+++ src/main/java/org/virep/jdabot/commands/VolumeCommand.java
@@ -0,0 +1,65 @@
+package org.virep.jdabot.commands;
+
+import lavalink.client.player.LavalinkPlayer;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.GuildVoiceState;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import org.virep.jdabot.lavaplayer.AudioManagerController;
+import org.virep.jdabot.lavaplayer.GuildAudioManager;
+import org.virep.jdabot.slashcommandhandler.SlashCommand;
+
+import java.util.Objects;
+
+public class VolumeCommand extends SlashCommand {
+    public VolumeCommand() {
+        super("volume", "Changes volume for the current queue.", new OptionData[] {
+                new OptionData(OptionType.INTEGER, "value", "Volume value", true)
+        });
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        Guild guild = event.getGuild();
+
+        GuildAudioManager manager = AudioManagerController.getGuildAudioManager(guild);
+        LavalinkPlayer player = manager.getPlayer();
+
+        assert guild != null;
+
+        final GuildVoiceState selfVoiceState = guild.getSelfMember().getVoiceState();
+        final GuildVoiceState memberVoiceState = Objects.requireNonNull(event.getMember()).getVoiceState();
+
+        assert memberVoiceState != null;
+        assert selfVoiceState != null;
+
+        if (memberVoiceState.getChannel() == null) {
+            event.reply("You are not in a voice channel!").setEphemeral(true).queue();
+            return;
+        }
+
+        if (!selfVoiceState.inAudioChannel()) {
+            event.reply("I'm currently not playing any music!").setEphemeral(true).queue();
+            return;
+        }
+
+        if (Objects.requireNonNull(selfVoiceState.getChannel()).getIdLong() != memberVoiceState.getChannel().getIdLong()) {
+            event.reply("You are not in the same channel as me!").setEphemeral(true).queue();
+            return;
+        }
+
+        OptionMapping valueOption = event.getOption("value");
+        assert valueOption != null;
+
+        int value = valueOption.getAsInt();
+
+        if (value < 0 || value > 100) {
+            event.reply("The volume value must be between 0 and 100.").setEphemeral(true).queue();
+        }
+
+        player.setVolume(value);
+        event.replyFormat("Volume has been set to %d", value).queue();
+    }
+}
