8d57dfdd206329994ea24d0358bc2fab6808d3ec
Alice <atamatori56@gmail.com>
2022-07-01 16:18:14 +0200

    chore: updated MariaDB JDBC Driver to 3.0.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ dependencies {
     implementation("com.github.chocololat:Lavalink-Client:fad198746c")
     implementation("org.reflections:reflections:0.10.2")
     implementation("com.github.Topis-Lavalink-Plugins:Topis-Source-Managers:2.0.6")
-    implementation("org.mariadb.jdbc:mariadb-java-client:3.0.4")
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.0.6")
     implementation("com.googlecode.json-simple:json-simple:1.1.1")
     implementation("org.apache.commons:commons-lang3:3.12.0")
 
