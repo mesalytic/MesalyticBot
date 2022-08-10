1a5cdfadc6d1a403feb2707d3085e7b9646b3884
Alice <atamatori56@gmail.com>
2022-08-10 02:59:08 +0200

    chore: updated to JDA alpha17

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,9 @@ repositories {
 }
 
 dependencies {
-    implementation("net.dv8tion:JDA:5.0.0-alpha.16")
+    implementation("net.dv8tion:JDA:5.0.0-alpha.17")
     implementation("io.github.cdimascio:java-dotenv:5.2.2")
-    implementation("com.github.chocololat:Lavalink-Client:38c6f65371")
+    implementation("com.github.chocololat:Lavalink-Client:03e6fcd027")
     implementation("org.reflections:reflections:0.10.2")
     implementation("com.github.Topis-Lavalink-Plugins:Topis-Source-Managers:2.0.6")
     implementation("org.mariadb.jdbc:mariadb-java-client:3.0.6")
@@ -22,8 +22,8 @@ dependencies {
     implementation("org.apache.commons:commons-lang3:3.12.0")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.10")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
 
 }
 
