3928586588bcd2552be9fd84d712e3890cee9b28
Alice <atamatori56@gmail.com>
2022-08-20 23:10:46 +0200

    feat: add OsuCommand

---
+++ src/main/java/org/virep/jdabot/commands/general/OsuCommand.java
@@ -0,0 +1,81 @@
+package org.virep.jdabot.commands.general;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import net.dv8tion.jda.internal.interactions.CommandDataImpl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.virep.jdabot.slashcommandhandler.Command;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+public class OsuCommand implements Command {
+    @Override
+    public String getName() {
+        return "osu";
+    }
+
+    @Override
+    public CommandData getCommandData() {
+        return new CommandDataImpl(getName(), "Gets stats from Osu!")
+                .addOptions(
+                        new OptionData(OptionType.STRING, "username", "The username of the profile", true),
+                        new OptionData(OptionType.STRING, "mode", "The game mode.", true).addChoices(
+                                new net.dv8tion.jda.api.interactions.commands.Command.Choice("Standard", "osu"),
+                                new net.dv8tion.jda.api.interactions.commands.Command.Choice("Taiko", "taiko"),
+                                new net.dv8tion.jda.api.interactions.commands.Command.Choice("Mania", "mania"),
+                                new net.dv8tion.jda.api.interactions.commands.Command.Choice("Catch The Beat", "ctb")
+                        )
+                );
+    }
+
+    @Override
+    public boolean isDev() {
+        return false;
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        event.deferReply().queue();
+        String username = Objects.requireNonNull(event.getOption("username")).getAsString();
+        String mode = Objects.requireNonNull(event.getOption("mode")).getAsString();
+
+        OkHttpClient client = new OkHttpClient.Builder()
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build();
+
+        try {
+            Request request = new Request.Builder()
+                    .url("https://api.mesavirep.xyz/osu?user=" + username + "&mode=" + mode)
+                    .build();
+
+            Response res = client.newCall(request).execute();
+
+            assert res.body() != null;
+
+            JSONObject jsonObject = new JSONObject(Objects.requireNonNull(res.body()).string());
+            JSONArray jsonBody = jsonObject.getJSONObject("body").getJSONArray("data");
+
+            byte[] bytes = new byte[jsonBody.length()];
+
+            for (int i = 0; i < jsonBody.length(); i++) {
+                bytes[i] = (byte)(((int)jsonBody.get(i)) & 0xFF);
+            }
+
+            InputStream targetBytes = new ByteArrayInputStream(bytes);
+
+            event.getHook().editOriginal(targetBytes, "osu-" + username + jsonObject.get("ext")).queue();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
