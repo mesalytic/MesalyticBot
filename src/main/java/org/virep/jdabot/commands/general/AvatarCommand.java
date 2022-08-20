9604e1bdd8311b7b1da87d70d09b19edc5f86657
Alice <atamatori56@gmail.com>
2022-08-20 12:53:41 +0200

    feat: add AvatarCommand

---
+++ src/main/java/org/virep/jdabot/commands/general/AvatarCommand.java
@@ -0,0 +1,37 @@
+package org.virep.jdabot.commands.general;
+
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.internal.interactions.CommandDataImpl;
+import org.virep.jdabot.slashcommandhandler.Command;
+
+public class AvatarCommand implements Command {
+    @Override
+    public String getName() {
+        return "avatar";
+    }
+
+    @Override
+    public CommandData getCommandData() {
+        return new CommandDataImpl(getName(), "Displays the avatar for the specified user.")
+                .addOption(OptionType.USER, "user", "The user to display.");
+    }
+
+    @Override
+    public boolean isDev() {
+        return false;
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent event) {
+        OptionMapping userOption = event.getOption("user");
+        User user = userOption != null ? userOption.getAsUser() : event.getUser();
+
+        String avatar = user.getEffectiveAvatarUrl();
+
+        event.reply("Here is **" + user.getAsTag() + "** avatar:\n\n" + avatar).queue();
+    }
+}
