013c3aa501b33bc01624f21ec054bcdd6436794e
Alice <atamatori56@gmail.com>
2022-09-27 17:04:26 +0200

    feat(AvatarCommand): add guild pfp support

--- src/main/java/org/virep/jdabot/commands/general/AvatarCommand.java
+++ src/main/java/org/virep/jdabot/commands/general/AvatarCommand.java
@@ -1,5 +1,6 @@
 package org.virep.jdabot.commands.general;
 
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
@@ -17,7 +18,8 @@ public class AvatarCommand implements Command {
     @Override
     public SlashCommandData getCommandData() {
         return Commands.slash(getName(), "Displays the avatar for the specified user.")
-                .addOption(OptionType.USER, "user", "The user to display.");
+                .addOption(OptionType.USER, "user", "The user to display.")
+                .addOption(OptionType.BOOLEAN, "guild", "Display guild profile picture");
     }
 
     @Override
@@ -28,10 +30,11 @@ public class AvatarCommand implements Command {
     @Override
     public void execute(SlashCommandInteractionEvent event) {
         OptionMapping userOption = event.getOption("user");
-        User user = userOption != null ? userOption.getAsUser() : event.getUser();
+        Member member = userOption != null ? userOption.getAsMember() : event.getMember();
+        boolean guild = event.getOption("guild") != null ? event.getOption("guild", OptionMapping::getAsBoolean) : false;
 
-        String avatar = user.getEffectiveAvatarUrl();
+        String avatar = guild ? member.getEffectiveAvatarUrl() : member.getUser().getAvatarUrl();
 
-        event.reply("Here is **" + user.getAsTag() + "** avatar:\n\n" + avatar).queue();
+        event.reply("Here is **" + member.getUser().getAsTag() + "** avatar:\n\n" + avatar).queue();
     }
 }
