8410e4be0fa337edd05f099efab0491fb0c8c0a7
chocololat <lgurvan81@gmail.com>
2023-10-16 22:23:25 +0200

    ref(RegisterCommand): adapted to text commands

--- src/main/java/org/virep/jdabot/textcommands/RegisterCommand.java
+++ src/main/java/org/virep/jdabot/textcommands/RegisterCommand.java
@@ -1,63 +1,49 @@
 package org.virep.jdabot.textcommands;
 
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
-import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import org.virep.jdabot.database.Database;
+import org.virep.jdabot.handlers.TextCommand;
 import org.virep.jdabot.language.Language;
-import org.virep.jdabot.handlers.SlashCommand;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collections;
-import java.util.List;
 
-public class RegisterCommand implements SlashCommand {
+public class RegisterCommand implements TextCommand {
     @Override
     public String getName() {
         return "register";
     }
-
-    @Override
-    public SlashCommandData getCommandData() {
-        return Commands.slash(getName(), "Register and start your Melyca Adventure!");
-    }
-
-    @Override
-    public List<Permission> getBotPermissions() {
-        return Collections.emptyList();
-    }
-
     @Override
     public boolean isDev() {
         return true;
     }
 
     @Override
-    public void execute(SlashCommandInteractionEvent event) {
+    public void execute(MessageReceivedEvent event, String[] args) throws SQLException {
         Guild guild = event.getGuild();
+        Message message = event.getMessage();
 
         try (Connection connection = Database.getConnection(); PreparedStatement statement = connection.prepareStatement("SELECT * FROM inventory WHERE userID = ?")) {
-            statement.setString(1, event.getUser().getId());
+            statement.setString(1, event.getAuthor().getId());
 
             ResultSet result = statement.executeQuery();
 
             if (result.first()) {
-                event.reply(Language.getString("REGISTER_ALREADY_REGISTERED", guild)).queue();
+                message.reply(Language.getString("REGISTER_ALREADY_REGISTERED", guild)).queue();
 
                 return;
             }
 
             PreparedStatement insertStatement = connection.prepareStatement("INSERT INTO inventory (userID) VALUES (?)");
-            insertStatement.setString(1, event.getUser().getId());
+            insertStatement.setString(1, event.getAuthor().getId());
 
             insertStatement.executeUpdate();
 
-            event.reply(Language.getString("REGISTER_REGISTERED", guild)).queue();
+            message.reply(Language.getString("REGISTER_REGISTERED", guild)).queue();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
