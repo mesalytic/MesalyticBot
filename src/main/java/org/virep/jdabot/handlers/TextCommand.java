271fa7bdb6b50a89bf202fddc6464235f2916da4
chocololat <lgurvan81@gmail.com>
2023-10-16 22:23:10 +0200

    chore: added SQLException to the TextCommand class

--- src/main/java/org/virep/jdabot/handlers/TextCommand.java
+++ src/main/java/org/virep/jdabot/handlers/TextCommand.java
@@ -2,10 +2,12 @@ package org.virep.jdabot.handlers;
 
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
+import java.sql.SQLException;
+
 public interface TextCommand {
     public String getName();
 
     public boolean isDev();
 
-    public void execute(MessageReceivedEvent event, String[] args);
+    public void execute(MessageReceivedEvent event, String[] args) throws SQLException;
 }
